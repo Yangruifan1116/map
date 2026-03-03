3178570de88c9946b6321388acf162e4dae390ed
YANG RUIFAN <1352081639@qq.com>
2026-03-03 15:18:25 +0800

    Add files via upload

---
+++ echarts-map-master (1)/echarts-map-master/README.md
@@ -0,0 +1,14 @@
+# echarts-map
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Result
+![效果图](public/result.jpg)
---
+++ echarts-map-master (1)/echarts-map-master/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <echarts-map></echarts-map>
+  </div>
+</template>
+<script>
+import EchartsMap from './views/EchartsMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EchartsMap
+  }
+}
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
